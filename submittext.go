02bd131bfc30007ca27d815598fc7fc41221045a
Chris Siannas <xrsia@otenet.gr>
2022-01-19 12:54:02 +0200

    getUserText with 2 strings returned

--- submittext.go
+++ submittext.go
@@ -9,16 +9,20 @@ import(
 
 func main(){
 
-	getUserText()
-
+	file,text :=getUserText()
+	fmt.Println("The file name is: ", file)
+	fmt.Println("The text to be saved is:",text)
+	
 }
 
-func getUserText(){
+func getUserText()(string,string){
 	terminalReader := bufio.NewReader(os.Stdin)
 	fmt.Print("Enter a single word as a file name: ")
 	filename, _ := terminalReader.ReadString('\n')
 	filename = strings.TrimSpace(filename) 
 	filename += ".txt"
 
-	fmt.Println("The filename is: ", filename)
+	fmt.Print("Enter the text to be saved:")
+	myText, _ := terminalReader.ReadString('\n')
+	return filename, myText
 }
