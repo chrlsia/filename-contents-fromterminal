7d018bf5ab27ea731a1886fd45a01b9caec422d5
Chris Siannas <xrsia@otenet.gr>
2022-01-19 12:38:27 +0200

    Start

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import(
+   "bufio"
+    "fmt"
+   "os"
+   "strings"
+)
+
+func main(){
+
+	terminalReader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter a single word as a file name: ")
+	filename, _ := terminalReader.ReadString('\n')
+	filename = strings.TrimSpace(filename) 
+	filename += ".txt"
+
+	fmt.Println("The filename is: ", filename)
+
+}
