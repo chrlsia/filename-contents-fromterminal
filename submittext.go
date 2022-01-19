c34073f858401612e138417b1f3631f7f1a18937
Chris Siannas <xrsia@otenet.gr>
2022-01-19 18:03:48 +0200

    Final

--- submittext.go
+++ submittext.go
@@ -12,7 +12,17 @@ func main(){
 	file,text :=getUserText()
 	fmt.Println("The file name is: ", file)
 	fmt.Println("The text to be saved is:",text)
-	
+	saveText(text, file)
+}
+
+func saveText(myText string, myFile string) {
+	//fileHandler, _ := os.Create(myFile)
+	fileHandler, _ := os.OpenFile(myFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)	
+	defer fileHandler.Close()
+	myWriterBelt := bufio.NewWriter(fileHandler)
+	fmt.Fprintln(myWriterBelt, myText)
+	myWriterBelt.Flush()
+	fmt.Println("Data has been saved")
 }
 
 func getUserText()(string,string){
